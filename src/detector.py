5553ba3779cadfb7102a1e49efaf46a2517154da
string <pcollie4@vols.utk.edu>
2022-11-16 17:28:49 -0500

    basic reader

--- src/detector.py
+++ src/detector.py
@@ -1,28 +1,33 @@
 import psutil
 import time
+import sys
 
 class Counter():
-    def __init__(self) -> None:
+    def __init__(self, length: int, filename: str) -> None:
+        self.length = length
+        self.data = []
+        self.file = filename
         self.run()
 
     def run(self) -> None:
 
-        c = 1
-        data = []
         names = ["read_count", "write_count", "read_bytes", "write_bytes", "read_time", "write_time", "read_merged_count", "write_merged_count", "busy_time"]
         for item in self.count():
-            data.append(item)
+            self.data.append([item])
         
         while(1):
             counts = self.count()
-            c += 1
             print(counts)
-            for i, item in enumerate(counts):
-                data[i] += item
 
-            with open("avgs.csv", "w") as avgfile:
-                for j, item in enumerate(data):
-                    avgfile.write("{}: {:.2f} \n".format(names[j], item/c))
+            self.update(counts)
+
+            with open(self.file + '.csv', "w") as avgfile:
+                for i, avg in enumerate(self.data):
+                    a = 0
+                    for x in avg:
+                        a += x
+                    a /= len(avg)
+                    avgfile.write("{}, {:.2f}\n".format(names[i], a))
 
             time.sleep(1)
 
@@ -31,6 +36,13 @@ class Counter():
 
         return counts
 
+    def update(self, counts: tuple) -> None:
+        for i, item in enumerate(counts):
+            self.data[i].append(item)
+            if len(self.data[i]) > self.length:
+                self.data[i].pop(0)
+        
+
 
 if __name__  == "__main__":
-    c = Counter()
+    c = Counter(5, sys.argv[1])
